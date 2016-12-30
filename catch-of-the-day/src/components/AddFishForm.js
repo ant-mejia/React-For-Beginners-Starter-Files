9b5399c6200c0f3717e588334b6959c61d5b657e
ant-mejia <ps@antmejia.com>
2016-12-30 15:14:49 -0500

    Video Completed: Understanding State

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+  createFish(event) {
+    event.preventDefault();
+    console.log('Gonna Make Some Fish!');
+    const fish = {
+      name: this.name.value,
+      price: this.price.value,
+      status: this.status.value,
+      desc: this.desc.value,
+      image: this.image.value,
+    }
+    this.props.addFish(fish);
+    this.fishForm.reset();
+  }
+
+  render() {
+    return (
+      <form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.createFish(e)}>
+        <input type="text" ref={(input) => this.name = input} placeholder="Fish Name"/>
+        <input type="text" ref={(input) => this.price = input} placeholder="Fish Price"/>
+        <select ref={(input) => this.status = input}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea ref={(input) => this.desc = input}></textarea>
+        <input type='text' ref={(input) => this.image = input} placeholder="Fish Image"/>
+        <button type='submit'>+ Add Item</button>
+      </form>
+    )
+  }
+}
+
+export default AddFishForm;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {BrowserRouter, Match, Miss,} from 'react-router';
+import {BrowserRouter, Match, Miss} from 'react-router';
 import './css/style.css';
-import App from './components/app';
+import App from './components/App';
 import StorePicker from './components/StorePicker';
 import NotFound from './components/NotFound';
 const Root = () => {
